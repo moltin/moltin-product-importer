11e26e8b6a7e0dad79ab7d60f0879b6eb954ed51
Matthew Foyle <matt@moltin.com>
2019-07-04 14:24:41 +0100

    Add CLI

---
+++ src/cli.js
@@ -0,0 +1,55 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import chooseAndRunImport from './main'
+
+async function promptForMissingOptions(options) {
+    const defaultTemplate = 'products';
+    
+    if (options.skipPrompts) {
+      return {
+        ...options,
+        template: options.template || defaultTemplate,
+      };
+    }
+   
+    const questions = [];
+    if (!options.entity) {
+      questions.push({
+        type: 'list',
+        name: 'entity',
+        message: 'Please choose which entity to import',
+        choices: ['products'],
+        default: defaultTemplate,
+      });
+    }
+   
+    const answers = await inquirer.prompt(questions);
+
+    return {
+      ...options,
+      entity: options.entity || answers.entity,
+    };
+   }
+
+function parseArgumentsIntoOptions(rawArgs) {
+ const args = arg(
+   {
+     '--entity': String,
+     '-e': '--entity',
+     '--yes': Boolean
+   },
+   {
+     argv: rawArgs.slice(2),
+   }
+ );
+ return {
+   entity: args['--entity'],
+   skipPrompts: args['--yes'] || false,
+ };
+}
+
+export async function cli(args) {
+ let options = parseArgumentsIntoOptions(args);
+ options = await promptForMissingOptions(options);
+ await chooseAndRunImport(options.entity)
+}
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import chalk from 'chalk';
+
+export default async function chooseAndRunImport(entity) {
+
+ switch(entity) {
+    case 'products':
+        require('./products/index')
+        break
+    default:
+        console.log('entity not available for import')
+ }
+
+ console.log('%s import is ready to run', chalk.green.bold(entity));
+ return
+}
